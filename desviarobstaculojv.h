1f91fbcc333ea9c20c331adaffd1efd924a179bf
robotica@sc111443 <robotica@sc111443>
2018-05-14 17:44:59 -0300

    Melhorei o código de desviar obstáculo.

--- desviarobstaculojv.h
+++ desviarobstaculojv.h
@@ -6,33 +6,40 @@ void desviarObstaculo(){
 
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
+
 		OnFwd(MOTOR_DIR,80);
 //		Wait(4000);
-		OnRev(MOTOR_ESQ,75);
-		Wait (1043);
+		OnRev(MOTOR_ESQ,85);
+		Wait (1250);
+		
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
 		OnFwd(MOTOR_DIR_ESQ,75);
-		Wait (1500);
+		Wait (1550);
+		
 		OnFwd(MOTOR_DIR,-80);
 		OnRev(MOTOR_ESQ,-75);
-		Wait (1043);
+		Wait (1250);
+		
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
 		OnFwd(MOTOR_DIR_ESQ,75);
-		Wait (2000);
+		Wait (2450);
+		
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
 		OnFwd(MOTOR_DIR,-80);
 		OnRev(MOTOR_ESQ,-75);
-		Wait (1043);
+		Wait (1050);
+		
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);	
 		OnFwd(MOTOR_DIR_ESQ,75);
-		Wait (1000);
+		Wait (1400);
+		
 		OnFwd(MOTOR_DIR,80);
 		OnRev(MOTOR_ESQ,75);
-		Wait (1043);
+		Wait (1350);
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
 }
