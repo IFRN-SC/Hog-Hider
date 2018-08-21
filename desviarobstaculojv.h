b9d2a174d9564382cadb8d0e2e64cce1c4cc6d5f
robotica@sc111497 <robotica@sc111497>
2018-08-21 17:32:46 -0300

    Melhorias:
    Criamos funções que separam as etapas do desvio de obstaculo e alteramos os valores de tempo de execucao dos passos.

--- desviarobstaculojv.h
+++ desviarobstaculojv.h
@@ -2,45 +2,113 @@
 #define MOTOR_DIR OUT_A
 #define MOTOR_DIR_ESQ OUT_AC
 
-
-
-	void girarEsquerda(){
-		RotateMotor(MOTOR_DIR_ESQ, 75, 45); 
-
-	}
-
-
-		//OnFwd(MOTOR_DIR_ESQ,0);
-/*
+void paraGirarEsquerda(){
+		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
+
 		OnFwd(MOTOR_DIR,80);
-		OnRev(MOTOR_ESQ,75);
-		Wait (1043);
+//		Wait(4000);
+		OnRev(MOTOR_ESQ,85);
+		Wait (1250);	
+}
+
+
+
+void paraFrente(){
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
 		OnFwd(MOTOR_DIR_ESQ,75);
-		Wait (1500);
+		Wait (1550);
+}
+
+
+void girarDireita(){
 		OnFwd(MOTOR_DIR,-80);
 		OnRev(MOTOR_ESQ,-75);
-		Wait (1043);
+		Wait (1250);
+}
+
+
+void paraFrente2(){
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
 		OnFwd(MOTOR_DIR_ESQ,75);
-		Wait (2000);
+		Wait (2550);
+}
+
+
+void paraGirarDireita(){
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
 		OnFwd(MOTOR_DIR,-80);
 		OnRev(MOTOR_ESQ,-75);
-		Wait (1043);
+		Wait (1100);
+}
+
+void paraFrente3(){
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);	
 		OnFwd(MOTOR_DIR_ESQ,75);
-		Wait (1000);
+		Wait (1750);
+}
+
+void girarEsquerdaPara(){
 		OnFwd(MOTOR_DIR,80);
 		OnRev(MOTOR_ESQ,75);
-		Wait (1043);
+		Wait (1200);
+		OnFwd(MOTOR_DIR_ESQ,0);
+		Wait (700);
+}
+
+
+void desviarObstaculo(){
+		/*
+		OnFwd(MOTOR_DIR_ESQ,0);
+		Wait (700);
+
+		OnFwd(MOTOR_DIR,80);
+//		Wait(4000);
+		OnRev(MOTOR_ESQ,85);
+		Wait (1250);
+		
+		OnFwd(MOTOR_DIR_ESQ,0);
+		Wait (700);
+		OnFwd(MOTOR_DIR_ESQ,75);
+		Wait (1550);
+		
+		OnFwd(MOTOR_DIR,-80);
+		OnRev(MOTOR_ESQ,-75);
+		Wait (1250);
+		
+		OnFwd(MOTOR_DIR_ESQ,0);
+		Wait (700);
+		OnFwd(MOTOR_DIR_ESQ,75);
+		Wait (2450);
+		
+		OnFwd(MOTOR_DIR_ESQ,0);
+		Wait (700);
+		OnFwd(MOTOR_DIR,-80);
+		OnRev(MOTOR_ESQ,-75);
+		Wait (1050);
+		
+		OnFwd(MOTOR_DIR_ESQ,0);
+		Wait (700);	
+		OnFwd(MOTOR_DIR_ESQ,75);
+		Wait (1400);
+		
+		OnFwd(MOTOR_DIR,80);
+		OnRev(MOTOR_ESQ,75);
+		Wait (1350);
 		OnFwd(MOTOR_DIR_ESQ,0);
 		Wait (700);
 */
+paraGirarEsquerda();
+paraFrente();
+girarDireita();
+paraFrente2();
+paraGirarDireita();
+paraFrente3();
+girarEsquerdaPara();
+} 
 
 
